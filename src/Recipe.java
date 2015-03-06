d584ba707342066710f20201a5849abab9b4785a
toshi0383 <t.suzuki326@gmail.com>
2015-03-06 11:07:50 +0900

    spec1: add Recipe.java

---
+++ src/Recipe.java
@@ -0,0 +1,13 @@
+/**
+ * オムライスと出力する
+ * @author toshi0383
+ */
+public class Recipe {
+	/**
+	 * メイン処理
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("オムライス");
+	}
+}
