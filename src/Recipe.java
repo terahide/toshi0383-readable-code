6896d0cec4999327aa1e7e893a17569eaef564e4
toshi0383 <t.suzuki326@gmail.com>
2015-03-06 11:53:33 +0900

    spec3: 引数のファイルのレシピを出力するようにした

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,13 +1,60 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * オムライスと出力する
  * @author toshi0383
  */
 public class Recipe {
+	
+	private final static String USAGE = "java Recipe [fileName]";
 	/**
 	 * メイン処理
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("オムライス");
+		if (args == null || args.length == 0) {
+			System.out.println("Usage:" + USAGE);
+			return;
+		}
+		String fileName = args[0];
+		if (!new File(fileName).exists()) {
+			System.out.println("No such file found:" + fileName);
+			return;
+		}
+		for (String recipe : getLinesFromFileName(fileName)) {
+			System.out.println(recipe);
+		}
+	}
+	/**
+	 * 引数のファイル名の中身を１行ずつList にいれて返す
+	 * @param recipeFileName
+	 * @return
+	 */
+	@SuppressWarnings("resource")
+	public static List<String> getLinesFromFileName(String recipeFileName) {
+		List<String> recipes = new ArrayList<String>();
+		FileReader reader;
+		BufferedReader br;
+		try {
+			File file = new File(recipeFileName);
+			if (!file.exists()) {
+				return recipes;
+			}
+			reader = new FileReader(file);
+			br = new BufferedReader(reader);
+		} catch(IOException e) { return recipes; }
+
+		try {
+			String s;
+			while ((s = br.readLine()) != null) {
+				recipes.add(s);
+			}
+		} catch(IOException e) { return recipes; }
+		return recipes;
 	}
 }
